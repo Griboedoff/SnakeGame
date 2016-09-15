044532f2101b342d07d5ddedab49665cd05d4f21
Griboedoff <igormb@MacBook-Pro-IgorMb.local>
2016-09-15 15:55:41 +0500

    Add LevelRepo

---
+++ src/Model/Infrastructure/LevelRepo.java
@@ -0,0 +1,27 @@
+package Model.Infrastructure;
+
+import Model.Level;
+
+import java.io.File;
+import java.util.Objects;
+
+public class LevelRepo {
+	private File levelsDir;
+
+	public LevelRepo(String path) {
+		levelsDir = new File(path);
+		if (!levelsDir.isDirectory())
+			throw new IllegalArgumentException(String.format("%s is not directory", path));
+	}
+
+	public Level GetLevel(String levelName) {
+		File[] levelsFiles = levelsDir.listFiles();
+		if (levelsFiles != null)
+			for (File levelFile : levelsFiles) {
+				if (Objects.equals(levelFile.getName(), levelName + ".txt")) {
+					return Level.CreateFromFile(levelFile);
+				}
+			}
+		throw new IllegalArgumentException(String.format("%s not found", levelName));
+	}
+}
--- src/Model/Level.java
+++ src/Model/Level.java
@@ -1,12 +1,20 @@
 package Model;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.io.File;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Level {
+public class Level implements Serializable {
 	private GameField Field;
 	ArrayList<Snake> Snakes;
 
 	public Level(int width, int height) {
 		Field = new GameField(width, height);
+	}
+
+	public static Level CreateFromFile(File file) {
+		throw new NotImplementedException();
 	}
 }
